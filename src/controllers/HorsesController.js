a9cdffc8dae338190231f2d2e412d9c52e6d4332
jaymeco <jayme.anunciacao@gmail.com>
2020-11-16 17:11:17 -0300

    Add horses controller

---
+++ src/controllers/HorsesController.js
@@ -0,0 +1,37 @@
+import { request } from 'express';
+
+const knex = require('../database');
+
+class HorsesController {
+  async create(request, response) {
+    try {
+      const {
+        name,
+        sex,
+        breed,
+        age,
+        weight,
+        height,
+        description,
+      } = request.body;
+
+      const horse = await knex('horses')
+        .insert({
+          name,
+          sex,
+          breed,
+          age,
+          weight,
+          height,
+          description,
+        })
+        .returning('*');
+
+      return response.status(201).json(horse);
+    } catch (error) {
+      response.status(400).json({ error });
+    }
+  }
+}
+
+export default HorsesController;
