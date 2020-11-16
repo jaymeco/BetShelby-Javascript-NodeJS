2d39842383eb5ed510cc16ab0b9153ccdef4f9f5
jaymeco <jayme.anunciacao@gmail.com>
2020-11-16 14:27:56 -0300

    Add app routes file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const routes = require('./routes/app.routes');
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 The server is running!');
