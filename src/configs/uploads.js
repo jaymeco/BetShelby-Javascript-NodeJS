a852545b24216e43f8140c7e647a7707c8d5fd81
jaymeco <jayme.anunciacao@gmail.com>
2020-11-16 21:31:03 -0300

    Add multer configuration

---
+++ src/configs/uploads.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+
+module.exports = {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    filename: (request, file, callback) => {
+      const filehash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${filehash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
