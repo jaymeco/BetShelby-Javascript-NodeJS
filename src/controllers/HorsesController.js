7cd04898ed2ef0549a232f38420b77b31bfe95c0
jaymeco <jayme.anunciacao@gmail.com>
2020-11-16 22:39:10 -0300

    Add creation of user on create horse method

--- src/controllers/HorsesController.js
+++ src/controllers/HorsesController.js
@@ -3,8 +3,11 @@ const knex = require('../database');
 
 class HorsesController {
   async create(request, response) {
+    const trx = await knex.transaction();
+
     try {
       const {
+        user,
         name,
         sex,
         breed,
@@ -13,6 +16,42 @@ class HorsesController {
         height,
         description,
       } = request.body;
+
+      const userExits = await knex('users')
+        .where({ email: user.email })
+        .select();
+
+      if (!userExits.length) {
+        const user_id = uuid.v4();
+
+        const userCreated = await trx('users')
+          .insert({
+            id: user_id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+          })
+          .returning('id');
+
+        const horseId = uuid.v4();
+
+        const horseCreated = await trx('horses')
+          .insert({
+            id: horseId,
+            name,
+            sex,
+            breed,
+            age,
+            weight,
+            height,
+            user_id: userCreated[0],
+            description,
+          })
+          .returning('*');
+
+        await trx.commit();
+        return response.status(201).json(horseCreated);
+      }
 
       const id = uuid.v4();
 
@@ -25,12 +64,14 @@ class HorsesController {
           age,
           weight,
           height,
+          user_id: userExits[0].id,
           description,
         })
         .returning('*');
 
       return response.status(201).json(horse);
     } catch (error) {
+      await trx.rollback();
       response.status(400).json({ error });
     }
   }
--- src/routes/horse.routes.js
+++ src/routes/horse.routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const multer = require('multer');
 const HorsesController = require('../controllers/HorsesController');
+const uploadConfig = require('../configs/uploads');
 
 const horsesRoutes = express.Router();
 
 const horsesController = new HorsesController();
 
-horsesRoutes.post('/', horsesController.create);
+const upload = multer(uploadConfig);
+
+horsesRoutes.post('/', upload.array('images'), horsesController.create);
 
 module.exports = horsesRoutes;
