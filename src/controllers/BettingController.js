2fc45cbde70079fadc6fc4dba789823f3713ac70
jaymeco <jayme.anunciacao@gmail.com>
2020-11-18 13:23:51 -0300

    Add create method

--- src/controllers/BettingController.js
+++ src/controllers/BettingController.js
@@ -3,30 +3,53 @@ const knex = require('../database');
 
 class BettingController {
   async create(resquest, response) {
-    const {
-      user_name,
-      user_email,
-      user_phone,
-      bet_amount,
-      horses,
-    } = resquest.body;
-    const userExits = await knex('users').where({ email: user_email }).select();
+    const trx = knex.transaction();
 
-    if (!userExits.length) {
-      const user_id = uuid.v4();
+    const { userName, userEmail, userPhone, betAmount, horses } = resquest.body;
 
-      const userCreated = await trx('users')
-        .insert({
-          id: user_id,
-          name: user_name,
-          email: user_email,
-          phone: user_phone,
-        })
-        .returning('id');
+    try {
+      const userExits = await knex('users')
+        .where({ email: userEmail })
+        .select();
 
-      // horses.map(horse=>{
-      //   knex('')
-      // })
+      if (!userExits.length) {
+        const user_id = uuid.v4();
+
+        const userCreated = await trx('users')
+          .insert({
+            id: user_id,
+            name: userName,
+            email: userEmail,
+            phone: userPhone,
+          })
+          .returning('id');
+
+        const bettingId = uuid.v4();
+
+        horses.map(async horse => {
+          await knex('betting').insert({
+            id: bettingId,
+            bet_amount: betAmount,
+            horse_id: horse.id,
+            user_id: userCreated[0],
+          });
+        });
+      }
+
+      const id = uuid.v4();
+
+      horses.map(async horse => {
+        await knex('betting').insert({
+          id,
+          bet_amount: betAmount,
+          horse_id: horse.id,
+          user_id: userExits[0].id,
+        });
+      });
+
+      return response.status(201).json({ success: 'Aposta realizada!' });
+    } catch (error) {
+      return response.status(400).json({ error });
     }
   }
 }
